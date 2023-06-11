7a9f0132f6ecdf1c429e7677f0dcba829031f21f
raosan <raosanf.lillahi@gmail.com>
2023-06-11 21:02:53 +0700

    Initial Commit

---
+++ hnews/hnewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hnewsApp.swift
+//  hnews
+//
+//  Created by Raosan Fikri Lillahi on 11/06/23.
+//
+
+import SwiftUI
+
+@main
+struct hnewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
