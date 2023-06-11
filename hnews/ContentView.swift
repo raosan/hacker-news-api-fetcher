53bea38953b19f191e650da17b46ba0219e8c173
raosan <raosanf.lillahi@gmail.com>
2023-06-11 21:42:40 +0700

    apply observable

--- hnews/ContentView.swift
+++ hnews/ContentView.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
+            List(networkManager.posts) { post in
                 Text(post.title)
             }
             .navigationBarTitle("H News")
@@ -23,9 +25,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-let posts = [
-    Post(id: "1", title: "Hellow"),
-    Post(id: "2", title: "Halo"),
-    Post(id: "3", title: "Konichiwa"),
-]
